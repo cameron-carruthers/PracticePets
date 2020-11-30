c1c2bff5308e1c8551f351c0925c6a7f0a9f5082
Cameron Carruthers <cameronlcarruthers@gmail.com>
2020-11-29 20:57:39 -0700

    Re-seed database

---
+++ config/db.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => {
+    console.error(err.message);
+    process.exit(1);
+  });
+
+const { Schema } = mongoose;
+
+const studentSchema = new Schema({
+  name:  String,
+  gender: String,
+  points:   Number,
+  pets: [String],
+  weeklyPracticeData: [
+    {
+      dateSubmitted: { type: Date, default: Date.now },
+      practiceAmount: Number,
+      completedAssignments: Boolean,
+      Comments: String
+    }
+  ]
+});
+
+const Student = mongoose.model('student', studentSchema);
+Student.collection.createIndex({ name: 1 }, { unique: true });
+
+module.exports = Student;
