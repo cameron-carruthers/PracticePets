4699973d7b836fd4a624a43fb56c6a77752f0723
Cameron Carruthers <cameronlcarruthers@gmail.com>
2020-11-30 02:50:21 -0700

    Refactor to using Heroku environment variables

--- config/db.js
+++ config/db.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
-const config = require('config');
-const db = config.get('mongoURI');
+// const config = require('config');
+// const db = config.get('mongoURI');
+
+const db = process.env.DATABASE_URL;
 
 mongoose
   .connect(db, {
